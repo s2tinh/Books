e2d0453c1491b97e106ee59a63bc912be2804150
devtinh <tranvoanhtinh@gmail.com>
2025-02-06 07:43:17 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,4 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 
-
 }
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import fs from 'fs';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        host: '0.0.0.0', // Có thể sử dụng host này nếu muốn chạy từ nhiều địa chỉ IP.
-        port: 5173, // Cổng mà Vite sẽ chạy, bạn có thể thay đổi nếu cần.
-        // HTTPS không cần thiết với Koyed.
-    },
-});
+    server: command === 'serve' ? {
+        host: '0.0.0.0',
+        port: 5173,
+    } : undefined,
+}));
